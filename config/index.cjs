1dcef5b6cc8a9464a210b2f516531b5c4e0a84b3
destinyu <1191641830@qq.com>
2023-02-10 10:33:40 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -50,23 +50,17 @@ const USER_CONFIG = {
       horoscopeDate: '2-16',
       festivals: [
         // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
-        {
-          type: '*生日', name: '宝贝', year: '1996', date: '09-09',
-        },
+        
         // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
         {
-          type: '生日', name: '李四', year: '1996', date: '09-31',
+          type: '生日', name: '宝贝', year: '1998', date: '02-16',
         },
-        {
-          type: '节日', name: '相识纪念日', year: '2020', date: '09-03',
-        },
+        
       ],
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2022-06-23' },
-        // 结婚纪念日
-        { keyword: 'marry_day', date: '2022-09-09' },
       ],
     },
   ],
